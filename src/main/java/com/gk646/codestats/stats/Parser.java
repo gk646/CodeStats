91ee9a65063f046d83ea979f03343323618adcc1
gk646 <lukas.gilch@gmx.de>
2023-07-21 15:04:16 +0200

    added icons drawing added full table infod added overview table added row sorting added support for loading large files optimized performance half way -> very performant already
    added listeners for project open and toolwindow open
    full settings support
    full persistent save support
    
    75% finished

--- src/main/java/com/gk646/codestats/stats/Parser.java
+++ src/main/java/com/gk646/codestats/stats/Parser.java
@@ -1,34 +1,103 @@
 package com.gk646.codestats.stats;
 
+import com.gk646.codestats.CodeStatsWindow;
 import com.gk646.codestats.settings.Save;
-import com.intellij.openapi.roots.ProjectFileIndex;
-import com.intellij.openapi.vfs.VirtualFile;
+import com.gk646.codestats.tabs.IconCellRenderer;
+import com.gk646.codestats.util.StringParsing;
+import com.intellij.icons.AllIcons;
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.openapi.progress.Task;
+import com.intellij.ui.components.JBScrollPane;
 import com.intellij.ui.table.JBTable;
+import org.jetbrains.annotations.NotNull;
 
-import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
-import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
+import java.io.File;
 import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.charset.MalformedInputException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
-import java.util.List;
+import java.util.HashSet;
+import java.util.stream.Stream;
 
 public class Parser {
-    public ProjectFileIndex projectIndex;
-    HashMap<String, OverViewEntry> overView = new HashMap<>(6);
-    ArrayList<HashMap<String, StatEntry>> tabs = new ArrayList<>(6);
+    public String projectPathString;
+    public Path projectPath;
+    HashSet<String> excludedTypes = new HashSet<>(16);
+    HashSet<String> excludedDirs = new HashSet<>(16);
+    HashSet<String> separateTabs = new HashSet<>(16);
+    HashMap<String, OverViewEntry> overView = new HashMap<>(10);
+    HashMap<String, ArrayList<StatEntry>> tabs = new HashMap<>(6);
 
-    public Parser(ProjectFileIndex pfi) {
-        this.projectIndex = pfi;
-        for (var ext : Save.getInstance().separateTabsTypes.split(";")) {
-            overView.put(ext, new OverViewEntry());
+    public Parser(String path) {
+        this.projectPath = Path.of(path);
+        this.projectPathString = projectPath.toString();
+        updateState();
+    }
+
+    public void updateState() {
+        var save = Save.getInstance();
+        overView.clear();
+        excludedTypes.clear();
+        excludedDirs.clear();
+
+        Collections.addAll(excludedTypes, save.excludedFileTypes.split(";"));
+        Collections.addAll(separateTabs, save.separateTabsTypes.split(";"));
+
+        for (var dir : save.excludedDirectories) {
+            excludedDirs.add(Path.of(dir).toString());
+        }
+
+        if (save.exclude_idea) {
+            excludedDirs.add(projectPath + File.separator + ".idea");
+        }
+        if (save.exclude_npm) {
+            excludedDirs.add(projectPath + File.separator + "node_modules");
+        }
+        if (save.exclude_compiler) {
+            excludedDirs.add(projectPath + File.separator + "out");
+            excludedDirs.add(projectPath + File.separator + "cmake-build-debug");
+            excludedDirs.add(projectPath + File.separator + "cmake-build-release");
+        }
+        if (save.exclude_git) {
+            excludedDirs.add(projectPath + File.separator + ".git");
         }
     }
 
-    public void update(JTabbedPane pane) {
-        iterateFiles();
-        rebuildTabbedPane(pane);
+    public void updatePane(JTabbedPane pane) {
+        Task.Backgroundable task = new Task.Backgroundable(CodeStatsWindow.project, "Updating Code Stats", false) {
+            @Override
+            public void run(@NotNull ProgressIndicator indicator) {
+                var time = System.currentTimeMillis();
+                resetCache();
+                iterateFiles();
+
+                ApplicationManager.getApplication().invokeLater(() -> {
+                    rebuildTabbedPane(pane);
+                    var duration = System.currentTimeMillis() - time;
+                    var notification = new Notification("CodeStats", "Code Stats", "Update completed in " + String.format("%.4f", duration / 1_000f) + " sec.", NotificationType.INFORMATION);
+                    notification.setIcon(AllIcons.General.Information);
+                    Notifications.Bus.notify(notification);
+                });
+            }
+        };
+        ProgressManager.getInstance().run(task);
     }
 
     private void rebuildTabbedPane(JTabbedPane pane) {
@@ -36,57 +105,201 @@ public class Parser {
         int i = 0;
         for (var pair : overView.entrySet()) {
             var entry = pair.getValue();
-            data[i] = new Object[]{pair.getKey(), entry.count, entry.linesCode};
+            data[i] = new Object[]{pair.getKey(), entry.count + "x", entry.sizeSum + "kb", entry.sizeMin + "kb", entry.sizeMax + "kb", entry.sizeAvg + "kb", entry.lines, entry.linesMin, entry.linesMax, entry.linesAvg, entry.linesCode};
+            i++;
         }
-        String[] columnNames = {"Extension", "Count", "Lines"};
-        JTable table = new JBTable(new DefaultTableModel(data, columnNames));
-        pane.addTab("OverView", new JScrollPane(table));
-        /*
-        for (StatEntry statEntry : tabs) {
-            Object[][] data = {
-                    {"Total Lines", statEntry.totalLines()},
-                    {"Source Code Lines", statEntry.sourceCodeLines()},
-                    {"Comment Lines", statEntry.commentLines()},
-                    {"Blank Lines", statEntry.blankLines()}
-            };
-            String[] columnNames = {"Stat", "Value"};
+        String[] columnNames = {"Extension", "Count", "Size SUM", "Size MIN", "Size MAX", "Size AVG", "Lines", "Lines MIN", "Lines MAX", "Lines AVG", "Lines CODE"};
 
-            JTable table = new JBTable(new DefaultTableModel(data, columnNames));
+        DefaultTableModel model = new DefaultTableModel(data, columnNames);
+        var table = new JBTable(model);
 
-            pane.addTab(statEntry.name(), new JScrollPane(table));
-        }*/
+        table.setEnableAntialiasing(true);
+        table.setStriped(false);
+        table.setBorder(null);
+        table.getTableHeader().setReorderingAllowed(false);
+
+        TableRowSorter<TableModel> sorter = new TableRowSorter<>(model);
+
+        sorter.setComparator(1, Comparator.comparingInt(a -> Integer.parseInt(((String) a).replace("x", ""))));
+        sorter.setComparator(2, Comparator.comparingInt(a -> Integer.parseInt(((String) a).replace("kb", ""))));
+        sorter.setComparator(3, Comparator.comparingInt(a -> Integer.parseInt(((String) a).replace("kb", ""))));
+        sorter.setComparator(4, Comparator.comparingInt(a -> Integer.parseInt(((String) a).replace("kb", ""))));
+        sorter.setComparator(5, Comparator.comparingInt(a -> Integer.parseInt(((String) a).replace("kb", ""))));
+
+        sorter.setComparator(6, Comparator.comparingInt(a -> (Integer) a));
+        sorter.setComparator(7, Comparator.comparingInt(a -> (Integer) a));
+        sorter.setComparator(8, Comparator.comparingInt(a -> (Integer) a));
+        sorter.setComparator(9, Comparator.comparingInt(a -> (Integer) a));
+        sorter.setComparator(10, Comparator.comparingInt(a -> (Integer) a));
+
+
+        table.setRowSorter(sorter);
+        for (int columnIndex = 0; columnIndex < table.getColumnCount(); columnIndex++) {
+            table.getColumnModel().getColumn(columnIndex).setCellRenderer(new IconCellRenderer());
+        }
+
+        pane.addTab("OverView", new JBScrollPane(table));
+
+        for (var pair : tabs.entrySet()) {
+            var fileList = pair.getValue();
+            if (fileList.isEmpty()) continue;
+
+            data = new Object[fileList.size()][];
+            i = 0;
+            for (var entry : fileList) {
+                data[i] = new Object[]{entry.name, entry.totalLines, entry.sourceCodeLines, (int) (entry.sourceCodeLines * 100.0f / entry.totalLines + 0.5) + "%",
+                        entry.commentLines, (int) (entry.commentLines * 100.0f / entry.totalLines + 0.5) + "%", entry.blankLines, (int) (entry.blankLines * 100.0f / entry.totalLines + 0.5) + "%"
+                };
+                i++;
+            }
+            columnNames = new String[]{"Source File", "Total Lines", "Source Code Lines", "Source Code Lines [%]", "Comment Lines", "Comment Lines[%]", "Blank LInes", "Blank Lines [%]"};
+
+
+            model = new DefaultTableModel(data, columnNames);
+            table = new JBTable(model);
+
+            table.setEnableAntialiasing(true);
+            table.setStriped(false);
+            table.setBorder(null);
+            table.getTableHeader().setReorderingAllowed(false);
+
+            sorter = new TableRowSorter<>(model);
+
+            sorter.setComparator(1, Comparator.comparingInt(a -> (Integer) a));
+            sorter.setComparator(2, Comparator.comparingInt(a -> (Integer) a));
+            sorter.setComparator(4, Comparator.comparingInt(a -> (Integer) a));
+            sorter.setComparator(6, Comparator.comparingInt(a -> (Integer) a));
+
+            sorter.setComparator(3, Comparator.comparingInt(a -> Integer.parseInt(((String) a).replace("%", ""))));
+            sorter.setComparator(5, Comparator.comparingInt(a -> Integer.parseInt(((String) a).replace("%", ""))));
+            sorter.setComparator(7, Comparator.comparingInt(a -> Integer.parseInt(((String) a).replace("%", ""))));
+
+            table.setRowSorter(sorter);
+            pane.addTab(pair.getKey(), new JBScrollPane(table));
+        }
     }
 
-    private void parseFile(VirtualFile file) {
-        try {
-            System.out.println(file.getExtension());
-            var overview = overView.get(file.getExtension());
-            overview.count++;
+    private void parseFile(File file, String extension) {
+        overView.computeIfAbsent(extension, k -> new OverViewEntry());
 
-            byte[] bytes = file.contentsToByteArray();
-            String content = new String(bytes, file.getCharset());
+        if (separateTabs.contains(extension)) {
+            ArrayList<StatEntry> files = tabs.get(extension);
+            var entry = new StatEntry(file.getName());
+            try {
+                String content = Files.readString(file.toPath());
 
-            String[] lines = content.split("\n");
-            for (String line : lines) {
-                overview.lines++;
-                if (line.trim().isEmpty()) {
-                } else if (line.trim().startsWith("//")) {
-                    System.out.println("Single line comment found");
-                } else if (line.trim().startsWith("/*") && line.trim().endsWith("*/")) {
-                    System.out.println("Block comment found");
+                String[] lines = content.split("\n");
+                entry.totalLines += lines.length;
+                for (String line : lines) {
+                    if (line.trim().isEmpty()) {
+                        entry.blankLines++;
+                    } else if (line.trim().startsWith("//")) {
+                        entry.commentLines++;
+                    } else if (line.trim().startsWith("/*") && line.trim().endsWith("*/")) {
+                        entry.commentLines++;
+                    }
                 }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
+            entry.sourceCodeLines = entry.totalLines - entry.blankLines - entry.commentLines;
+
+            var overview = overView.get(extension);
+            overview.count++;
+            int size = (int) (file.length() / 1000);
+            overview.sizeSum += size;
+            if (size < overview.sizeMin) {
+                overview.sizeMin = size;
+            }
+            if (size > overview.sizeMax) {
+                overview.sizeMax = size;
+            }
+
+            overview.lines += entry.totalLines;
+            if (entry.totalLines < overview.linesMin) {
+                overview.linesMin = entry.totalLines;
+            }
+            if (entry.totalLines > overview.linesMax) {
+                overview.linesMax = entry.totalLines;
+            }
+            overview.linesCode += entry.sourceCodeLines;
+
+            files.add(entry);
+        } else {
+            try {
+                var overview = overView.get(extension);
+                overview.count++;
+
+                int lines = 0;
+                int size = (int) (file.length() / 1000);
+
+                if (size > 50000) {
+                    try (Stream<String> lineStream = Files.lines(file.toPath())) {
+                        lines = (int) lineStream.count();
+                    } catch (UncheckedIOException | IOException ignored) {
+                    }
+                } else {
+                    try {
+                        lines = Files.readString(file.toPath()).split("\n").length;
+                    } catch (MalformedInputException ignored) {
+                    }
+                }
+
+                overview.lines += lines;
+                if (lines < overview.linesMin) {
+                    overview.linesMin = lines;
+                }
+                if (lines > overview.linesMax) {
+                    overview.linesMax = lines;
+                }
+                overview.linesCode += lines;
+
+
+                overview.sizeSum += size;
+                if (size < overview.sizeMin) {
+                    overview.sizeMin = size;
+                }
+                if (size > overview.sizeMax) {
+                    overview.sizeMax = size;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void iterateFiles() {
+        try {
+            Files.walkFileTree(projectPath, new SimpleFileVisitor<>() {
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
+                    if (excludedDirs.contains(dir.toString())) {
+                        return FileVisitResult.SKIP_SUBTREE;
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) {
+                    var file = path.toFile();
+                    String extension = StringParsing.getFileExtension(file.getName());
+                    if (!extension.isEmpty() && !excludedTypes.contains(extension)) {
+                        parseFile(file, extension);
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+            });
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void iterateFiles() {
-        final List<String> excludedTypes = Save.getInstance().excludedDirectories;
-        projectIndex.iterateContent(file -> {
-            System.out.println(file.getName());
-            parseFile(file);
-            return true;
-        }, file -> file.isDirectory() && !excludedTypes.contains(file.getExtension()));
+    private void resetCache() {
+        overView.clear();
+        tabs.clear();
+
+        for (final var tab : separateTabs) {
+            tabs.put(tab, new ArrayList<>());
+        }
     }
 }
